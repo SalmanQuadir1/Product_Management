b69762abe8eb520e582c99f3692bf5bbf3dc2ce8
salman <najarsalman4@gmail.com>
2023-01-27 10:18:54 +0530

    salman

--- ProductManagement/src/main/java/com/productManagement/demo/entity/Products.java
+++ ProductManagement/src/main/java/com/productManagement/demo/entity/Products.java
@@ -10,43 +10,64 @@ public class Products {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long product_id;
-	private String product_name;
-	private String product_description;
-	private Double product_price;
+	private Long id;
+	private String name;
+	private String description;
+	private Double price;
 	private String status;
 	
 	
+	
+	public Products() {
+		super();
+	}
+	
+	public Products(Long id, String name, String description, Double price, String status) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "Products [id=" + id + ", name=" + name + ", description=" + description + ", price=" + price
+				+ ", status=" + status + "]";
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
 	public String getStatus() {
 		return status;
 	}
 	public void setStatus(String status) {
 		this.status = status;
 	}
-	public Long getProduct_id() {
-		return product_id;
-	}
-	public void setProduct_id(Long product_id) {
-		this.product_id = product_id;
-	}
-	public String getProduct_name() {
-		return product_name;
-	}
-	public void setProduct_name(String product_name) {
-		this.product_name = product_name;
-	}
-	public String getProduct_description() {
-		return product_description;
-	}
-	public void setProduct_description(String product_description) {
-		this.product_description = product_description;
-	}
-	public Double getProduct_price() {
-		return product_price;
-	}
-	public void setProduct_price(Double product_price) {
-		this.product_price = product_price;
-	}
+	
+	
 	
 	
 }
