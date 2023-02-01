24df84bbae5b35de11553ef1132c4d42e422c6a7
Aamir Nazir Kuchay <aamir.redstaglabs@gmail.com>
2023-02-01 14:08:53 +0530

    msg

--- ProductManagement/src/main/java/com/productManagement/demo/entity/User.java
+++ ProductManagement/src/main/java/com/productManagement/demo/entity/User.java
@@ -43,7 +43,7 @@ public class User {
 	private String location;
 	
 	@Column(name = "country")
-	private String country;
+	private String country;   
 
 	
 	@ManyToMany(cascade = {CascadeType.MERGE, CascadeType.PERSIST,CascadeType.REFRESH},fetch=FetchType.EAGER)
