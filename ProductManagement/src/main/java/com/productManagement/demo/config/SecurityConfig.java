68004cc93dccb384b22897d4ff12d5f3e4b66651
Aamir Nazir Kuchay <aamir.redstaglabs@gmail.com>
2023-02-24 15:42:55 +0530

    cors 2

--- ProductManagement/src/main/java/com/productManagement/demo/config/SecurityConfig.java
+++ ProductManagement/src/main/java/com/productManagement/demo/config/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     String[] permitted={"/api/**","/product/**"};
         http
+
                 .cors()
                 .and()
                 .csrf()
